d3fbc218107b968ea12a9aae10ce42cd0e460b33
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2024-04-08 10:21:23 +0100

    feat: add dynamic menu button and link based on page location

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import Header from "./component/layout/Header";
 import Login from "./component/pages/Login"
 import SignUp from "./component/pages/SignUp"
 import Profile from "./component/pages/Profile"
-import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header />
       <Router>
+        <Header />
         <Routes>
           <Route index element={<Login/>} />
           <Route path="/sign-up" element={<SignUp/>} />
--- src/component/layout/Header.jsx
+++ src/component/layout/Header.jsx
@@ -1,5 +1,23 @@
+import { useNavigate, useLocation } from "react-router-dom"
 
 function Header() {
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    let btnText, targetRoute
+
+    if(location.pathname === "/sign-up"){
+        btnText = "Login"
+        targetRoute = "/"
+    } else {
+        btnText = "Sign Up"
+        targetRoute = "/sign-up"
+    }
+
+    const handleClick = () => {
+        navigate(targetRoute)
+    }
+
   return (
     <>
         <div className="navbar bg-[#AA4A44]">
@@ -29,7 +47,7 @@ function Header() {
                 </ul>
             </div> */}
             <div className="navbar-end">
-                <a className="btn">Login</a>
+                <a className="btn" onClick={handleClick}>{btnText}</a>
             </div>
         </div>
     </>
