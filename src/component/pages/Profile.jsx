f981266581a1e54ff4fbc6efac75c9022b7e2ae1
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2024-04-08 11:44:03 +0100

    feat: add logout feature to profile page

--- src/component/pages/Profile.jsx
+++ src/component/pages/Profile.jsx
@@ -1,15 +1,27 @@
 /* eslint-disable react/prop-types */
+import { useNavigate } from "react-router-dom"
+import { auth } from "../../firebase"
+import { signOut } from "firebase/auth"
 
-function Profile({user}) {
+function Profile() {
+  const navigate = useNavigate()
+  const user = auth.currentUser
+
+  const logoutUser = async (e) => {
+    e.preventDefault()
+
+    await signOut(auth)
+    navigate("/")
+  }
   return (
     <>
       <div className="card w-96 bg-base-100 shadow-xl">
-        <h2 className="card-title">Welcome {user}</h2>
+        <h2 className="card-title">Welcome {user.email}</h2>
             <div className="card-body">
               
               <p>you are logged in</p>
               <div className="card-actions justify-end">
-                <button className="btn btn-primary">Log Out</button>
+                <button className="btn btn-primary" onClick={(e) => logoutUser(e)}>Log Out</button>
               </div>
             </div>
         </div>
